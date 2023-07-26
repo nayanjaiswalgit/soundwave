c9722c6b23ba1ab906190bb44333de46fcf3a4e9
Nayan Jaiswal <nayan.jaiswal@coriolis.co.in>
2023-07-26 14:33:17 +0530

    add responsiveness

--- src/slices/authSlice.js
+++ src/slices/authSlice.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { fetchData } from "../api/spotifyAPI";
+import { getAccessTokenFromRefreshToken } from "../utils/AuthorizationPage";
 
 const initialState = {
   isAuthenticated: false,
@@ -7,12 +8,18 @@ const initialState = {
   token: null,
   error: null,
 };
-export const UserProfile = createAsyncThunk(
-    'user/auth',
-    async () => {
-    return fetchData("https://api.spotify.com/v1/me")
-    }
-  );
+
+export const UserProfile = createAsyncThunk("user/auth", async () => {
+  return fetchData("https://api.spotify.com/v1/me");
+});
+
+export const refreshAccessTokenAsync = createAsyncThunk(
+  "user/refreshToken",
+  async () => {
+    return getAccessTokenFromRefreshToken();
+  }
+);
+
 const authSlice = createSlice({
   name: "auth",
   initialState,
@@ -23,31 +30,62 @@ const authSlice = createSlice({
       state.token = action.payload.token;
     },
     LogOut(state) {
-      (state.isAuthenticated = false),
-        state.user = null ;
-        state.token = null;
-        localStorage.clear();
+      state.isAuthenticated = false;
+      state.user = null;
+      state.token = null;
+      localStorage.clear();
     },
   },
   extraReducers: (builder) => {
     builder
       .addCase(UserProfile.pending, (state) => {
-       
         state.error = null;
-
       })
       .addCase(UserProfile.fulfilled, (state, action) => {
-      
         state.user = action.payload;
-     
-        state.isAuthenticated = true
+        state.isAuthenticated = true;
+
+        if (action.payload.expires_in) {
+          const expirationTime = Date.now() + action.payload.expires_in * 1000;
+
+          localStorage.setItem("tokenExpirationTime", expirationTime);
+        }
       })
       .addCase(UserProfile.rejected, (state, action) => {
         state.isAuthenticated = false;
         state.error = action.error.message;
+      })
+      .addCase(refreshAccessTokenAsync.fulfilled, (state, action) => {
+        
+        state.token = action.payload.token;
+        
+        if (action.payload.expires_in) {
+          const expirationTime = Date.now() + action.payload.expires_in * 1000;
+          localStorage.setItem("tokenExpirationTime", expirationTime);
+        }
+      })
+      .addCase(refreshAccessTokenAsync.rejected, (state, ) => {
+       
+        state.isAuthenticated = false;
+        state.user = null;
+        state.token = null;
+        localStorage.clear();
       });
   },
 });
 
 export default authSlice.reducer;
 export const { loginSuccess, LogOut } = authSlice.actions;
+
+
+export const checkTokenExpiration = () => (dispatch, getState) => {
+  const { token } = getState().auth;
+  const tokenExpirationTime = localStorage.getItem("tokenExpirationTime");
+
+  if (token && tokenExpirationTime) {
+    const expirationTime = parseInt(tokenExpirationTime, 10);
+    if (Date.now() >= expirationTime) {
+      dispatch(refreshAccessTokenAsync());
+    }
+  }
+};
